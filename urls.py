663226c3f68096664bdf71ac2b4f5ef2d9fe73b2
Tan Sze Chuen <tan@szechuen.com>
2012-04-08 08:44:08 +0800

    Added Logout view

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
 	(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+	(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
 	(r'^member/$', login_required(MemberListView.as_view())),
 	(r'^event/$', login_required(EventListView.as_view())),
 )
