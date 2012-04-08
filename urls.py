8b8debcb7d77fc4e63ebe69d37bd0175755393d4
Tan Sze Chuen <tan@szechuen.com>
2012-04-08 08:29:17 +0800

    Added login_required for existing views

--- urls.py
+++ urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url, include
 from openduty.views import *
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
 	(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-	(r'^member/$', MemberListView.as_view()),
-	(r'^event/$', EventListView.as_view()),
+	(r'^member/$', login_required(MemberListView.as_view())),
+	(r'^event/$', login_required(EventListView.as_view())),
 )
