1ba0f93574d4fc4287b4491997a981eed32af00f
Tan Sze Chuen <tan@szechuen.com>
2012-04-08 08:16:10 +0800

    Added Login view

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url, include
 from openduty.views import *
 
 urlpatterns = patterns('',
+	(r'^accounts/login/$', 'django.contrib.auth.views.login'),
 	(r'^member/$', MemberListView.as_view()),
 	(r'^event/$', EventListView.as_view()),
 )
