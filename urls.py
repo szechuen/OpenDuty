a19cf1ce36468174a929793f97f23d394749e4a3
Tan Sze Chuen <tan@szechuen.com>
2012-04-20 21:39:17 +0800

    Added password change view and template

--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = patterns('',
 	(r'^accounts/login/$', 'django.contrib.auth.views.login'),
 	(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+	(r'^accounts/password_change/$', 'django.contrib.auth.views.password_change'),
 	(r'^member/$', login_required(MemberListView.as_view())),
 	(r'^event/$', login_required(EventListView.as_view())),
 )
