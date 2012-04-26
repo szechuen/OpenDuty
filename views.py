f08fddf76d40a8141d6cca451af8df9c9309f59c
Tan Sze Chuen <tan@szechuen.com>
2012-04-26 22:12:56 +0800

    Changed MemberCreateView to use MemberForm and creates User

--- views.py
+++ views.py
@@ -1,5 +1,10 @@
 from django.views.generic import *
+from django.forms import ModelForm
+from django import forms
 from openduty.models import *
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+import re
 
 class MemberListView(ListView):
 	model = Member
@@ -9,9 +14,27 @@ class MemberDetailView(DetailView):
 	model = Member
 	template_name = "member/detail.html"
 
+class MemberForm(ModelForm):
+	email = forms.EmailField()
+
+	class Meta:
+		model = Member
+		exclude = ('user',)
+
 class MemberCreateView(CreateView):
-	model = Member
+	form_class = MemberForm
 	template_name = "member/create.html"
+
+	def form_valid(self, form):
+		username = re.sub(r'\W', '', form.instance.name)
+		email = form.cleaned_data['email']
+		password = User.objects.make_random_password()
+
+		user = User.objects.create_user(username, email, password)
+		user.email_user("OpenDuty: User Created", render_to_string("member/create_email.html", {'username': username, 'password': password}))
+
+		form.instance.user = user
+		return super(MemberCreateView, self).form_valid(form)
 
 class EventListView(ListView):
 	model = Event
